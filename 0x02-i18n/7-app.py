10a5de2a73820d0b58449783c4445744e99b583a
RedolF <redolf639@gmail.com>
2024-05-22 21:30:09 +0000

    il8n

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -2,8 +2,8 @@
 """A Basic Flask app with internationalization support.
 """
 import pytz
+from flask_babel import Babel
 from typing import Union, Dict
-from flask_babel import Babel, format_datetime
 from flask import Flask, render_template, request, g
 
 
@@ -48,17 +48,11 @@ def before_request() -> None:
 def get_locale() -> str:
     """Retrieves the locale for a web page.
     """
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    locale = query_table.get('locale', '')
+    locale = request.args.get('locale', '')
     if locale in app.config["LANGUAGES"]:
         return locale
-    user_details = getattr(g, 'user', None)
-    if user_details and user_details['locale'] in app.config["LANGUAGES"]:
-        return user_details['locale']
+    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
+        return g.user['locale']
     header_locale = request.headers.get('locale', '')
     if header_locale in app.config["LANGUAGES"]:
         return header_locale
@@ -82,8 +76,7 @@ def get_timezone() -> str:
 def get_index() -> str:
     """The home/index page.
     """
-    g.time = format_datetime()
-    return render_template('index.html')
+    return render_template('7-index.html')
 
 
 if __name__ == '__main__':
